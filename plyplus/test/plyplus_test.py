576d2f66615cbfdc888cce730740cc63eaa6d21d
Erez Shin <erezshin+git@gmail.com>
2013-01-23 14:24:28 +0200

    Added unicode test, re-organized test modules

--- plyplus/test/plyplus_test.py
+++ plyplus/test/plyplus_test.py
@@ -2,15 +2,14 @@ from __future__ import absolute_import, print_function
 from io import open
 
 import unittest
-import sys, os, glob
+import os
 import logging
-import time
 import copy
 import pickle
-from plyplus import grammars
 from plyplus.plyplus import Grammar, TokValue, ParseError, STree
 
 from .selector_test import TestSelectors
+from .test_grammars import TestPythonG, TestConfigG
 
 logging.basicConfig(level=logging.INFO)
 
@@ -19,13 +18,6 @@ def _read(n, *args):
     with open(os.path.join(CUR_PATH, n), *args) as f:
         return f.read()
 
-if os.name == 'nt':
-    if 'PyPy' in sys.version:
-        PYTHON_LIB = os.path.join(sys.prefix, 'lib-python', sys.winver)
-    else:
-        PYTHON_LIB = os.path.join(sys.prefix, 'Lib')
-else:
-    PYTHON_LIB = '/usr/lib64/python2.7/'
 
 FIB = """
 def fib(n):
@@ -63,11 +55,18 @@ class TestPlyPlus(unittest.TestCase):
 
         g = Grammar("start: '\(' name_list (COMMA MUL NAME)? '\)'; @name_list: NAME | name_list COMMA NAME ;  MUL: '\*'; COMMA: ','; NAME: '\w+'; ")
         l2 = g.parse('(a,b,c,*x)')
-        assert l == l2, '%s != %s' % (l,l2)
+        assert l == l2, '%s != %s' % (l, l2)
+
+    def test_unicode(self):
+        g = Grammar(r"""start: UNIA UNIB UNIA;
+                    UNIA: '\xa3';
+                    UNIB: '\u0101';
+                    """)
+        g.parse(u'\xa3\u0101\u00a3')
 
 class TestSTrees(unittest.TestCase):
     def setUp(self):
-        self.tree1 = STree('a', [STree(x,y) for x,y in zip('bcd', 'xyz')])
+        self.tree1 = STree('a', [STree(x, y) for x, y in zip('bcd', 'xyz')])
 
     def test_deepcopy(self):
         assert self.tree1 == copy.deepcopy(self.tree1)
@@ -123,89 +122,6 @@ def test2():
     sexp = ['start',
              ]
         """, expected=18)
-
-class TestPythonG(unittest.TestCase):
-    def setUp(self):
-        with grammars.open('python.g') as g:
-            self.g = Grammar(g)
-
-    def test_basic1(self):
-        g = self.g
-        l = g.parse(_read('python_sample1.py'))
-        l = g.parse(_read('python_sample2.py'))
-        l = g.parse(_read('../../examples/calc.py'))
-        l = g.parse(_read('../grammar_lexer.py'))
-        l = g.parse(_read('../grammar_parser.py'))
-        l = g.parse(_read('../strees.py'))
-        l = g.parse(_read('../grammars/python_indent_postlex.py'))
-
-        l = g.parse(_read('../plyplus.py'))
-
-        l = g.parse("c,d=x,y=a+b\nc,d=a,b\n")
-
-    def test_weird_stuff(self):
-        g = self.g
-        for n in range(3):
-            if n == 0:
-                s = """
-a = \\
-        \\
-        1\\
-        +2\\
--3
-print a
-"""
-            elif n == 1:
-                s = "a=b;c=d;x=e\n"
-
-            elif n == 2:
-                s = r"""
-@spam3 (\
-this,\
-blahblabh\
-)
-def eggs9():
-    pass
-
-"""
-
-            g.parse(s)
-
-
-    def test_python_lib(self):
-        g = self.g
-
-        path = PYTHON_LIB
-        files = glob.glob(path+'/*.py')
-        start = time.time()
-        for f in files:
-            f2 = os.path.join(path,f)
-            logging.info( f2 )
-            l = g.parse(_read(f2))
-
-        end = time.time()
-        logging.info( "test_python_lib (%d files), time: %s secs"%(len(files), end-start) )
-
-    def test_python4ply_sample(self):
-        g = self.g
-        l = g.parse(_read(r'python4ply-sample.py'))
-
-
-class TestConfigG(unittest.TestCase):
-    def setUp(self):
-        with grammars.open('config.g') as g:
-            self.g = Grammar(g)
-
-    def test_config_parser(self):
-        g = self.g
-        res = g.parse("""
-            [ bla Blah bla ]
-            thisAndThat = hel!l%o/
-            one1111:~$!@ and all that stuff
-
-            [Section2]
-            whatever: whatever
-            """)
 
 if __name__ == '__main__':
     unittest.main()
