b6265b29a82202e62f84fd5933d106aaa1de2c3a
Erez Shin <erezshin+git@gmail.com>
2013-03-19 19:39:40 +0200

    Version 0.4.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "PlyPlus",
-    version = "0.4.5",
+    version = "0.4.6",
     packages = ['plyplus', 'plyplus.test', 'plyplus.grammars', 'examples', 'docs'], #find_packages(),
     #scripts = ['say_hello.py'],
 
