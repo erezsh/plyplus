5e1db7ec4fbd5174bc4efea0806ee2c115ed9293
Erez Shin <erezshin@gmail.com>
2013-05-25 18:26:37 +0300

    Refactored plyplus.test into __main__, test_parser, test_trees, etc.
    Now to run the entire test suite, run: python -m plyplus.test

--- plyplus/test/test_parser.py
+++ plyplus/test/test_parser.py
@@ -1,15 +1,11 @@
-from __future__ import absolute_import, print_function
-from io import open
+from __future__ import absolute_import
 
 import unittest
-import os
 import logging
-import copy
-import pickle
-from plyplus.plyplus import Grammar, TokValue, ParseError, STree
+import os
+from io import open
 
-from .selector_test import TestSelectors
-from .test_grammars import TestPythonG, TestConfigG
+from plyplus.plyplus import Grammar, TokValue, ParseError
 
 logging.basicConfig(level=logging.INFO)
 
@@ -63,36 +59,6 @@ class TestPlyPlus(unittest.TestCase):
                     UNIB: '\u0101';
                     """)
         g.parse(u'\xa3\u0101\u00a3')
-
-class TestSTrees(unittest.TestCase):
-    def setUp(self):
-        self.tree1 = STree('a', [STree(x, y) for x, y in zip('bcd', 'xyz')])
-
-    def test_deepcopy(self):
-        assert self.tree1 == copy.deepcopy(self.tree1)
-
-    def test_parents(self):
-        s = copy.deepcopy(self.tree1)
-        s.calc_parents()
-        for i, x in enumerate(s.tail):
-            assert x.parent() == s
-            assert x.index_in_parent == i
-
-    def test_pickle(self):
-        s = copy.deepcopy(self.tree1)
-        data = pickle.dumps(s)
-        assert pickle.loads(data) == s
-
-    def test_pickle_with_parents(self):
-        s = copy.deepcopy(self.tree1)
-        s.calc_parents()
-        data = pickle.dumps(s)
-        s2 = pickle.loads(data)
-        assert s2 == s
-
-        for i, x in enumerate(s2.tail):
-            assert x.parent() == s2
-            assert x.index_in_parent == i
 
 def test_python_lex(code=FIB, expected=54):
     g = Grammar(_read('python.g'))
