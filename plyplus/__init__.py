5a4c8cbe6931635924f234a83b3ff8fb3124476d
Erez Shinan <erezshin+git@gmail.com>
2015-05-25 16:42:04 +0300

    Version Bump

--- plyplus/__init__.py
+++ plyplus/__init__.py
@@ -6,7 +6,7 @@ from getpass import getuser
 import os
 from tempfile import gettempdir
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 PLYPLUS_DIR = os.path.join(gettempdir(), 'plyplus-' + getuser())
 
