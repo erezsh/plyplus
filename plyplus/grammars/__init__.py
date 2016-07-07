a61b838a36008f2e3d32509b1b723a7a9bdb1bd9
Erez Shinan <erezshin@gmail.com>
2016-07-07 19:13:06 +0300

    Added shortcuts to grammars

--- plyplus/grammars/__init__.py
+++ plyplus/grammars/__init__.py
@@ -4,3 +4,6 @@ from io import open as _open
 def open(grammar_name):
     return _open( os.path.join(os.path.dirname(__file__), grammar_name) )
 
+python = open('python.g')
+config = open('config.g')
+selector = open('selector.g')
